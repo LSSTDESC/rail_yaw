79a9418269339da42fda131facdd6cf1da5eaf00
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-05-10 10:38:25 +0000

    updated test data URL

--- src/rail/yaw_rail/utils.py
+++ src/rail/yaw_rail/utils.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import os
-import subprocess
-from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING
 
 from pandas import read_parquet
@@ -16,21 +13,5 @@ __all__ = [
 
 
 def get_dc2_test_data() -> DataFrame:
-    link = (
-        "https://drive.usercontent.google.com/"
-        "download?id=1n9_vfbTlUrlyR6eUwHxJLZ35OjYf8Uyf&export=download"
-    )
-    with TemporaryDirectory() as tmp_path:
-        path = os.path.join(tmp_path, "test_dc2_rail_yaw.pqt")
-        try:
-            subprocess.run(
-                ["curl", link, "-o", path],
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                timeout=10,
-            )
-            data = read_parquet(path)
-        except Exception as err:  # pragma: no cover
-            raise OSError("downloading test data failed") from err
-    return data
+    link = "https://portal.nersc.gov/cfs/lsst/PZ/test_dc2_rail_yaw.pqt"
+    return read_parquet(link)
