b774ad7fa7926bcc70a1086725879317370b64f7
Mubdi Rahman <mubdi@sidratresearch.com>
2025-12-18 16:39:02 -0500

    Importing stages to enable discoverability

--- src/rail/yaw_rail/__init__.py
+++ src/rail/yaw_rail/__init__.py
@@ -2,3 +2,7 @@
 The module `yaw_rail` implements utilities and data handles for the
 *yet_another_wizz* wrapper in `rail.estimation.algos.cc_yaw.py`.
 """
+
+from rail.estimation.algos.cc_yaw import *
+
+from ._version import __version__
